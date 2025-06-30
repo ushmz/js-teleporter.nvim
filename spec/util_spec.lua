2ed726b2cf7adff9cbb4622c9ffce73274272595
ushmz <44394399+ushmz@users.noreply.github.com>
2025-07-01 01:12:37 +0900

    fix: spec

--- spec/util_spec.lua
+++ spec/util_spec.lua
@@ -27,12 +27,26 @@ describe("#get_path_difference", function()
 
   it("should return same path if base path is not a parent directory", function()
     vim.cmd("cd spec")
-    assert.is_same(util.get_path_difference("fixtures/same_dir/src/index.ts", "fixtures/nearest_parent_dir/src"), "fixtures/same_dir/src/index.ts")
+    assert.is_same(
+      util.get_path_difference("fixtures/same_dir/src/index.ts", "fixtures/nearest_parent_dir/src"),
+      "fixtures/same_dir/src/index.ts"
+    )
   end)
 
   it("should return empty and print error message when base path is not a directory", function()
     vim.cmd("cd spec")
     spy(logger.print_err):called_with("Base path should be a directory: stories")
     assert.is_same(util.get_path_difference("src/path/to/index.ts", "stories"), "")
+  end)
+
+  it("should return the full_path when base_path is empty and full_path is not", function()
+    vim.cmd("cd spec")
+    spy(logger.print_err):called(0)
+    assert.is_same(util.get_path_difference("fixtures/index.ts", ""), "fixtures/index.ts")
+  end)
+
+  it("should return an empty string when both full_path and base_path are empty", function()
+    vim.cmd("cd spec")
+    assert.is_same(util.get_path_difference("", ""), "")
   end)
 end)
