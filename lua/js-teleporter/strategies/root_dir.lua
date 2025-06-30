7513112411e3dbb92122f9ba393aa55de77846db
ushmz <44394399+ushmz@users.noreply.github.com>
2025-07-01 01:53:25 +0900

    fix: root_dir strategy

--- lua/js-teleporter/strategies/root_dir.lua
+++ lua/js-teleporter/strategies/root_dir.lua
@@ -1,8 +1,10 @@
 ---@type TeleportStrategy
 local RootDirectory = {
-  --- Calculates the target file path by transforming a test/storybook file path
-  --- back to its original source file path.
-  --- It returns the full path if the resulting source file exists and is readable, otherwise `nil`.
+  --- Calculates the target file path by transforming a test/storybook file path back to its original source file path.
+  --- It identifies the special marker directory (e.g., '__tests__') relative to the project root,
+  --- replaces it with the main source directory name (e.g., 'src'), and removes a suffix from the filename.
+  --- It returns the full path if the resulting target directory exists, otherwise `nil`.
+  --- The existence of the file itself is not checked.
   ---
   --- Example:
   ---
@@ -10,8 +12,8 @@ local RootDirectory = {
   --- local context = { suffix = ".test", markers = { "__tests__" }, root = "src" }
   --- local path = "my_project/__tests__/utils/index.test.ts"
   ---
-  --- -- It will attempt to find "my_project/src/utils/"
-  --- -- If it exists, return "my_project/src/utils/index.ts" regardless its existence.
+  --- -- It will attempt to find "my_project/src/utils/".
+  --- -- If this directory exists, it returns "my_project/src/utils/index.ts".
   --- require("js-teleporter.strategy.root_dir").from(context, path)
   --- ```
   from = function(context, path)
@@ -22,13 +24,13 @@ local RootDirectory = {
     end
 
     local suffix = context.suffix or ""
-    local target_filename, count = filename:gsub(suffix .. "$", "", 1)
+    local target_filename, count = filename:gsub(vim.pesc(suffix) .. "$", "", 1)
     if count < 1 then
       return nil
     end
 
     for _, marker in ipairs(context.markers) do
-      local target_dir, cnt = dir:gsub("(/?)" .. marker .. "(/?)", "%1" .. context.root .. "%2", 1)
+      local target_dir, cnt = dir:gsub("(/?)" .. vim.pesc(marker) .. "(/?)", "%1" .. vim.pesc(context.root) .. "%2", 1)
       if cnt == 1 and vim.fn.isdirectory(target_dir) == 1 then
         return vim.fs.joinpath(target_dir, target_filename .. "." .. extension)
       end
@@ -36,19 +38,20 @@ local RootDirectory = {
 
     return nil
   end,
-  --- Calculates the target file path by transforming a source file path
-  --- to its corresponding test or storybook file path.
-  --- It iterates through `context.markers` and returns the first existing and readable target file.
+  --- Calculates the target file path by transforming a source file path to its corresponding test or storybook file path.
+  --- It replaces the main source directory (e.g., 'src') with one of the `context.markers` (e.g., '__tests__'),
+  --- and adds a suffix to the filename. It iterates through `context.markers` and returns the first target file path
+  --- whose parent directory exists. The existence of the file itself is not checked.
   ---
   --- Example:
   ---
   --- ```lua
-  --- local context = { suffix = ".test", markers = { "__tests__", "spec" } }
+  --- local context = { suffix = ".test", markers = { "__tests__", "spec" }, root = "src" }
   --- local path = "my_project/src/utils/index.ts"
   ---
   --- -- It will first attempt to find "my_project/__tests__/utils/" directory.
-  --- -- If it exists, return "my_project/__tests__/utils/index${suffix}.ts" regardless its existence.
-  --- -- If not found, it might then (if markers contains it) attempt to find "my_project/spec/utils/"
+  --- -- If it exists, it returns "my_project/__tests__/utils/index.test.ts".
+  --- -- If not found, it might then attempt to find "my_project/spec/utils/" directory.
   --- require("js-teleporter.strategy.root_dir").to(context, path)
   --- ```
   to = function(context, path)
@@ -61,9 +64,10 @@ local RootDirectory = {
     local target_filename = filename .. context.suffix
 
     for _, marker in ipairs(context.markers) do
-      local target_dir, count = dir:gsub("(.*)/" .. context.root, "%1/" .. marker, 1)
-      if count > 0 and vim.fn.isdirectory(target_dir) == 1 then
-        return vim.fs.joinpath(target_dir, target_filename .. "." .. extension)
+      ---@type string, string
+      local leading_seg, tailing_seg = dir:match("(.*)/" .. vim.pesc(context.root) .. "/(.*)", 1)
+      if vim.fn.isdirectory(vim.fs.joinpath(leading_seg, marker)) == 1 then
+        return vim.fs.joinpath(leading_seg, marker, tailing_seg, target_filename .. "." .. extension)
       end
     end
 
