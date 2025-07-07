611c32533265f9783322f4a47b261f06c001237a
ushmz <44394399+ushmz@users.noreply.github.com>
2025-07-08 00:31:52 +0900

    refactor: strategies spec

--- spec/strategies/same_dir_spec.lua
+++ spec/strategies/same_dir_spec.lua
@@ -13,35 +13,35 @@ describe("same_dir", function()
 
   describe("from", function()
     it("Return original file path", function()
-      vim.cmd("cd spec/fixtures/same_dir")
+      vim.cmd("cd spec/fixtures")
       local context = {
         suffix = ".test",
         markers = { "__tests__" },
       }
 
-      assert.is_same(same_dir_strategy.from(context, "src/index.test.ts"), "src/index.ts")
+      assert.is_same(same_dir_strategy.from(context, "src/lib/same_dir.test.ts"), "src/lib/same_dir.ts")
     end)
   end)
 
   describe("to", function()
     it("Return test file path", function()
-      vim.cmd("cd spec/fixtures/same_dir")
+      vim.cmd("cd spec/fixtures")
       local context = {
         suffix = ".test",
         markers = { "__tests__" },
       }
 
-      assert.is_same(same_dir_strategy.to(context, "src/index.ts"), "src/index.test.ts")
+      assert.is_same(same_dir_strategy.to(context, "src/lib/same_dir.ts"), "src/lib/same_dir.test.ts")
     end)
 
     it("Return story file path", function()
-      vim.cmd("cd spec/fixtures/same_dir")
+      vim.cmd("cd spec/fixtures")
       local context = {
         suffix = ".stories",
         markers = { "__stories__" },
       }
 
-      assert.is_same(same_dir_strategy.to(context, "src/index.tsx"), "src/index.stories.tsx")
+      assert.is_same(same_dir_strategy.to(context, "src/components/same_dir.tsx"), "src/components/same_dir.stories.tsx")
     end)
   end)
 end)
